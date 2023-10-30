a6bcbbd850b74f7d812e640d420366014d4c527d
unknown <sibikumar03@gmial.com>
2023-10-30 21:22:43 +0530

    build apk and changed the start button color green when stop the task

--- lib/controllers/providers/home_screen_provider.dart
+++ lib/controllers/providers/home_screen_provider.dart
@@ -36,47 +36,15 @@ class HomeScreenProvider extends ChangeNotifier {
         geofencingStatus = false;
         notifyListeners();
       }
-  
     });
 
     print("the geofencestatus is ${geofenceStatusStream.toString()}");
   }
 
-    stopFencing()async {
-      EasyGeofencing.stopGeofenceService();
-    }
+  stopFencing() async {
+     geofencingStatus = true;
+    EasyGeofencing.stopGeofenceService();
+    notifyListeners();
+  }
 
-
-  // getLocation() async {
-  //   try {
-  //     curentPosition = await Geolocator.getCurrentPosition();
-  //     var distance = calculateDistance(10.010092, 76.336313,
-  //         curentPosition!.latitude, curentPosition!.longitude);
-
-  //     if (distance >= 200) {
-  //       geofencingStatus = false;
-  //     } else {
-  //       geofencingStatus = true;
-  //     }
-
-  //     print("geofencingStatus is $geofencingStatus");
-
-  //     notifyListeners();
-  //   } catch (e) {
-  //     debugPrint("tge exeption iss $e");
-  //   }
-  // }
-
-  // double calculateDistance(lat1, lon1, lat2, lon2) {
-  //   var p = 0.0;
-  //   var a = 0.0;
-  //   p = 0.017453292519943295;
-  //   a = 0.5 -
-  //       cos((lat2 - lat1) * p) / 2 +
-  //       cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
-  //   debugPrint("the distance iss ${(12742 * asin(sqrt(a))) * 1000}");
-  //   return (12742 * asin(sqrt(a))) * 1000;
-  // }
-
-
-}
+ }
